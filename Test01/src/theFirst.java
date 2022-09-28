1340a6ec68795e9bd72ebdc937d121d35c6da14f
MingYu <MingYu@petalmail.com>
2022-09-28 15:50:49 +0800

    first commit

---
+++ Test01/src/theFirst.java
@@ -0,0 +1,11 @@
+/**
+ * @Author LiMingYu
+ * @Create 2022-09-28 15:48
+ */
+public class theFirst {
+
+    public static void main(String[] args) {
+        System.out.println("一蓑烟雨任平生");
+    }
+
+}
