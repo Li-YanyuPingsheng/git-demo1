5e9f28f19d37af1c5555adc711225fd8bd483bc0
Li-YanyuPingsheng <94470095+Li-YanyuPingsheng@users.noreply.github.com>
2022-09-28 16:21:51 +0800

    Update theFirst.java

--- Test01/src/theFirst.java
+++ Test01/src/theFirst.java
@@ -7,7 +7,7 @@ public class theFirst {
     public static void main(String[] args) {
         System.out.println("一蓑烟雨任平生");
         System.out.println("上传成功");
-        System.out.println("主分支");
+        System.out.println("two分支");
         //two分支
     }
 
